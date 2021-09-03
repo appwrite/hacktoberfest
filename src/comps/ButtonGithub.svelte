08304ddb0eaff221cd24aa92ca8f1189fa4b1d01
Matej Baco <matejbacocom@gmail.com>
2021-09-03 18:35:53 +0200

    Updated link to blank

--- src/comps/ButtonGithub.svelte
+++ src/comps/ButtonGithub.svelte
@@ -4,7 +4,7 @@
 	export let showBadge = false;
 </script>
 
-<a href="https://github.com/appwrite/appwrite/issues">
+<a target="_blank" href="https://github.com/appwrite/appwrite/issues">
 	<Button type="primary" icon="github" text="View all Hacktoberfest issues">
 		{#if showBadge}
 			<div
