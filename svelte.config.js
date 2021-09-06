cedfdaafa76fde3978243ab6e4efc9933474f9b2
Torsten Dittmann <torsten.dittmann@googlemail.com>
2021-09-06 14:37:34 +0200

    feat(svelte): move to static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,7 +10,7 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: vercel()
+		adapter: adapter()
 	}
 };
 
