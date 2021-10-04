e4fbfcbb5fec60211e1191ef28a1b308da25874c
Christy Jacob <christyjacob4@gmail.com>
2021-10-04 20:30:26 +0530

    feat: added appwrite stickers

--- src/routes/index/_rewards.svelte
+++ src/routes/index/_rewards.svelte
@@ -102,7 +102,7 @@
 
 			<div class="col-span-12 lg:col-span-4 flex flex-col">
 				<div class="w-full bg-[#20007F] aspect-w-4 aspect-h-3 rounded-xl">
-					<img src="/swag/place3.webp" class="rounded-xl object-cover" alt="Third place" />
+					<img src="/swag/tier2.webp" class="rounded-xl object-cover" alt="Third place" />
 				</div>
 				<div class="relative -mt-10 px-4 sm:px-8 lg:flex-1">
 					<div
@@ -127,7 +127,7 @@
 
 			<div class="col-span-12 lg:col-span-4 flex flex-col">
 				<div class="w-full bg-[#20007F] aspect-w-4 aspect-h-3 rounded-xl">
-					<img src="/swag/place4.webp" class="rounded-xl object-cover" alt="Fourth place" />
+					<img src="/swag/tier1.webp" class="rounded-xl object-cover" alt="Fourth place" />
 				</div>
 				<div class="relative -mt-10 px-4 sm:px-8 lg:flex-1">
 					<div
