445766298d7b2c8927e9a09246f4077522c094f9
Matt Davison <matt@mjdws.com>
2022-10-04 14:41:02 +0100

    Update _rewards.svelte

--- src/routes/index/_rewards.svelte
+++ src/routes/index/_rewards.svelte
@@ -17,9 +17,9 @@
 					<h3 class="mb-3 text-xl lg:text-[2rem] font-semibold">4 contributions or more</h3>
 
 					<p class="leading-relaxed">
-						Limited edition Appwrite Hacktoberfest T-shirt,<br />
-						Sticker pack,<br />
-						Socks.
+						Limited edition Appwrite Hacktoberfest T-shirt<br />
+						Sticker pack<br />
+						Socks
 					</p>
 				</div>
 			</div>
@@ -33,9 +33,9 @@
 						<h3 class="mt-10 mb-3 text-xl font-semibold">3 contributions</h3>
 
 						<p class="leading-relaxed">
-							Original Appwrite sticker pack,<br />
-							Limited edition Hacktoberfest sticker pack,<br />
-							Socks.
+							Original Appwrite sticker pack<br />
+							Limited edition Hacktoberfest sticker pack<br />
+							Socks
 						</p>
 					</div>
 				</div>
@@ -50,8 +50,8 @@
 						<h3 class="mt-10 mb-3 text-xl font-semibold">2 contributions</h3>
 
 						<p class="leading-relaxed">
-							Original Appwrite sticker pack,<br />
-							Limited edition Hacktoberfest sticker pack.
+							Original Appwrite sticker pack<br />
+							Limited edition Hacktoberfest sticker pack
 						</p>
 					</div>
 				</div>
@@ -65,7 +65,7 @@
 					<div class="lg:h-full w-full">
 						<h3 class="mt-[32px] mb-3 text-xl font-semibold">1 contribution</h3>
 
-						<p class="leading-relaxed">Original Appwrite sticker pack.</p>
+						<p class="leading-relaxed">Original Appwrite sticker pack</p>
 					</div>
 				</div>
 			</div>
